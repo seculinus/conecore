97fdee2189913127d9270ea36adbe6b9349c22a0
seculinus <66112305+seculinus@users.noreply.github.com>
2025-07-08 23:48:08 +0400

    feat(sidebar): add sub-directory path
     -Create dropdown menus Component ; -Create Hover animations ; -Working towards Active Task

---
+++ src/assets/icons/SubDirectoryIcon.jsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+
+function SubdirectoryArrowIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="60.75 22.675 51.19 100.48"
+      width="48px"
+      height="51.19px"
+      xmlns:bx="https://boxy-svg.com"
+      {...props}
+    >
+      <path fill="#d8d8d8" stroke="#000" d="M60.919 22.675L60.75 123.155" />
+      <path
+        d="M60.75 33.955l.018 7.096c.019 7.097.056 21.29 8.37 28.4 8.313 7.11 24.902 7.138 33.197 7.152l8.295.014"
+        bx:d="M 60.75 33.955 U 60.861 76.533 U 110.63 76.617 1@8c1ab8ac"
+        fill="none"
+        stroke="#000"
+      />
+    </svg>
+  )
+}
+
+export default SubdirectoryArrowIcon
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -11,48 +11,89 @@ import DecomposeIcon from '../assets/icons/DecomposeIcon';
 import ConeCoreIcon from '../assets/icons/ConeCoreIcon';
 import SingleArrowIcon from '../assets/icons/SingleArrowIcon';
 import DoubleArrowIcon from '../assets/icons/DoubleArrowIcon';
+import SubdirectoryArrowIcon from '../assets/icons/SubDirectoryIcon';
+
+
+
+const SubMenuComponent = ({noItems, isShown})=>{
+
+    const items = [];
+    for (let i = 0; i<= noItems; i++)
+        items.push(i);
+
+    return (
+        <ul className = {isShown? "sub-menu show" : "sub-menu"}>
+            <div>
+                {items.map(num =>
+                    <li key = {num}>
+                        <SubdirectoryArrowIcon></SubdirectoryArrowIcon> 
+                        <a href="#">Item{num}</a> 
+                    </li>
+                )}
+            </div>
+        </ul>
+    )
+}
+
+
+
+const DropDownMenuComponent = ({items, children, id}) => {
+
+    const [isShown , setShown] = useState(false);
+
+    function handleClick(){
+        setShown(!isShown);
+        // console.log(event.target.parentElement.parentElement.classList)
+        // isShown? subMenu.classList.remove("show") : subMenu.classList.add("show")
+        // event.target.parentElement.parentElement.classList.toggle('rotate')
+    }
+    
+    return (
+        <li numId ={id}>
+            <button 
+            className = { isShown? "dropdown-btn rotate":"dropdown-btn" }
+            onClick = {handleClick}
+            >
+                {children}
+            </button>
+            <SubMenuComponent noItems ={items} isShown={isShown}></SubMenuComponent>
+
+        </li>
+    )
+}
+
+
 const Layout = () =>{
 
     const toggleButton = document.getElementById('toggle-btn');
     const sidebar = document.getElementById('sidebar');
+
+    const [activeIndex, setActiveIndex] = useState(0);
+    const listItems = document.querySelectorAll(".navigation > li")
+
+    function handleActiveIndex(event){
+        console.log(activeIndex)
+        // listItems[activeIndex].classList.toggle('active');
+        const newIndex = event.target.parentElement.getAttribute("numid");
+        setActiveIndex(newIndex);
+        listItems[newIndex].classList.toggle('active');
+    }
 
     function toggleSideBar(){
         sidebar.classList.toggle('close');
         toggleButton.classList.toggle('rotate');
     }
 
-    const [isCalcDropped, setCalcDropped] = useState(false)
-    const [isPlotDropped, setPlotDropped] = useState(false)
-
-
-
-    // function toggleSubMenu(){
-    //      event.target.classList.toggle('show');
-    //      event.target.toggle('rotate');
-    // }
-
-    function handleCalcClick (){
-            setCalcDropped(!isCalcDropped);
-            const subMenu = document.querySelector(".sub-menu");
-            isCalcDropped? subMenu.classList.remove("show") : subMenu.classList.add("show")
-            event.target.parentElement.parentElement.classList.toggle('rotate')
-            // console.log(event.target.parentElement.parentElement.nextElementSibling) 
-            // event.target.parentElement.parentElement.nextElementSibling.classList.toggle('show');
-    }
-    const handlePlotClick = ()=>{
-            setPlotDropped(!isPlotDropped);
-            const subMenu = document.querySelectorAll(".sub-menu")[1];
-            isPlotDropped? subMenu.classList.remove("show") : subMenu.classList.add("show") 
-    }
-
     return(
         <>
         <nav id = 'sidebar'>
-        
-        <ul className = 'navigation' >
-            {/* <div>ConeCore</div> */}
+        <ul 
+        className = 'navigation' 
+        onClick={handleActiveIndex}
+        >
             <li className = 'logo'>
                 <ConeCoreIcon></ConeCoreIcon>
+                conecore
                 <button
                  id = "toggle-btn"
                 onClick={toggleSideBar}
@@ -60,82 +101,66 @@ const Layout = () =>{
                     <DoubleArrowIcon ></DoubleArrowIcon>
                 </button>
             </li>
-            <li className = 'active'>
+            <li className = 'active' numId = {1}>
                 <NavLink to = '/'>
                     <HomeIcon></HomeIcon>
                     <span>Home</span>
                 </NavLink>
             </li>
 
-            <li>
+            <li numId = {2}>
                 <NavLink to = '/Settings'>
                     <SettingsIcon></SettingsIcon>
                     <span>Settings</span>
                 </NavLink>
             </li>
 
-            <li>
+            <li numId = {3}>
                 <NavLink to = '/Load'>
                     <LoadIcon></LoadIcon>
                     <span>Load</span>
                 </NavLink>
             </li>
-
-            <li>
-                <button 
-                className = "dropdown-btn"
-                onClick = {handleCalcClick}
-                >
+            <DropDownMenuComponent
+                items = {3}
+                children = {
                     <NavLink to = '/Calc'>
                         <CalcIcon></CalcIcon>
                         <span>Calc</span>
                         <SingleArrowIcon></SingleArrowIcon>
                     </NavLink>
-                </button>
-                <ul className = "sub-menu">
-                    <div>
-                        <li> <a href="#">Item1</a> </li>
-                        <li> <a href="#">Item2</a> </li>
-                        <li> <a href="#">Item3</a> </li>
-                    </div>
-                </ul>
-            </li>
-            <li>
-                <button 
-                className = "dropdown-btn"
-                onClick={handlePlotClick}
-                >
-                    
+                }
+                numId = {4}
+            >
+
+            </DropDownMenuComponent>
+
+            <DropDownMenuComponent
+            key = {2}
+                items = {5}
+                children = {
+          
                     <NavLink to = '/Plot'>
                         <PlotIcon></PlotIcon>                    
                         <span>Plot</span>
                         <SingleArrowIcon></SingleArrowIcon>
                     </NavLink>
-                </button>
-                <ul className = "sub-menu">
-                    <div>
-                        <li> <a href="#">Item1</a> </li>
-                        <li> <a href="#">Item2</a> </li>
-                        <li> <a href="#">Item3</a> </li>
-                        <li> <a href="#">Item4</a> </li>
-                        <li> <a href="#">Item5</a> </li>
-                    </div>
-                </ul>
-            </li>
-            <li>
+                }
+                numId = {5}
+            ></DropDownMenuComponent>
+                
+            <li numId = {6}>
                 <NavLink to = '/Print'>
                     <PrintIcon></PrintIcon>
                     <span>Print</span>
                 </NavLink>
             </li>
-            <li>
+            <li numId = {7}>
                 <NavLink to = '/Decompose'>
                     <DecomposeIcon></DecomposeIcon>
                     <span>Decompose</span>
                 </NavLink>
             </li>
-
-
         </ul>
         </nav>
         <main>
