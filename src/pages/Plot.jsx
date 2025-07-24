8fec38090e50fa8e5b34782c1fadd402af8e1726
seculinus <66112305+seculinus@users.noreply.github.com>
2025-07-24 22:53:10 +0400

    init three js

--- src/pages/Plot.jsx
+++ src/pages/Plot.jsx
@@ -1,14 +1,54 @@
 import { parsePoints } from "../utils/point";
+
+import { useEffect } from "react";
+import * as THREE from 'three';
+
+
+
 const Plot = () =>{
 
     function handlePointClick(){
         const points = parsePoints();
-        console.log(points)
-     
+        console.log(points)     
     }
+
+    useEffect(()=>{
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+            50,
+            window.innerWidth/window.innerHeight,
+            1,
+            1000
+        );
+        camera.position.z = 97;
+        
+        const canvas = document.getElementById('myThreeJsCanvas')
+        const renderer = new THREE.WebGLRenderer({canvas, antialias:true})
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        // document.body.appendChild(renderer.domElement);
+
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+        ambientLight.castShadow = true;
+        scene.add(ambientLight)
+
+        const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+        const boxMaterial = new THREE.MeshNormalMaterial();
+        const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+        scene.add(boxMesh);
+        
+        const animate = () =>{
+            boxMesh.rotation.x += 0.01;
+            boxMesh.rotation.y += 0.01;
+            renderer.render(scene, camera)
+            window.requestAnimationFrame(animate);
+        };
+        animate()
+    }, [])
+
     return(
         <div className="container">
             <button className onClick={handlePointClick}>Push me for points</button>
+            <canvas id = 'myThreeJsCanvas'></canvas>
         </div>
     )
 };
