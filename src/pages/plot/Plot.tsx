f72f127778489934a9f61579f0b3cd014e957616
seculinus <66112305+seculinus@users.noreply.github.com>
2025-09-01 23:45:48 +0400

    set camera transform to x.z (1of2)

--- src/pages/plot/Plot.tsx
+++ src/pages/plot/Plot.tsx
@@ -1,11 +1,8 @@
 import { parsePoints } from "../../utils/point";
-import { Canvas, useThree} from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import { GridCells } from "./grid";
 import { useRef, useEffect, useState, Fragment } from "react";
-import {
-  OrbitControls,
-  Html,
-} from "@react-three/drei";
+import { OrbitControls, Html } from "@react-three/drei";
 import * as THREE from "three";
 import "./Plot.css";
 
@@ -21,34 +18,35 @@ function handlePointClick() {
 }
 
 //Any??
-function CursorCoords(){
-  
-  const [position, setPosition] = useState(new THREE.Vector4(0,0,0,0));
+function CursorCoords() {
+  const [position, setPosition] = useState(new THREE.Vector3(0, 0, 0));
   const camera = useThree((state) => state.camera);
-  const canvas = useThree((state)=> state.scene);  
+
+  useEffect(()=>{
+    const handleMouse = (e:MouseEvent) =>{
+      const mouse = new THREE.Vector3();
+      const mouseX  = Number((e.clientX / window.innerWidth ));
+      const mouseY = Number((e.clientY / window.innerHeight ));
+      mouse.set(mouseX,  ( camera.near + camera.far ) / ( camera.near - camera.far ), mouseY)
+      mouse.unproject(camera);
+      console.log("is cursor fired?");
+      setPosition(mouse);
+  }
+  document.addEventListener("mousemove", handleMouse);
+
+  return () =>{ document.removeEventListener("mousemove", handleMouse)}
+  },[])
+
   return (
-        <Html className="content" distanceFactor={7} position={[0,3,3]} >
-            <div onPointerMove={(e)=>{
-            e.stopPropagation();
-            const pos = new THREE.Vector4();
-            pos.x = Number(((e.clientX / window.innerWidth )).toFixed(2));
-            pos.y = Number(((e.clientY / window.innerHeight )).toFixed(2));
-            pos.z = 0;
-            pos.w = 1;
-            pos.applyMatrix4(camera.projectionMatrixInverse);
-            console.log("is curosr fiered?")
-            setPosition(pos);
-        }}>
-                {
-                `${position.x}:${position.y}`
-                }
-            </div>
-        </Html>
-    )
+    <Html className="content" distanceFactor={7} position={[0, 3, 3]}>
+      <div>
+        {`${(position.x).toFixed(2)}:${position.z.toFixed(2)}`}
+      </div>
+    </Html>
+  );
 }
 
 function CellBase({ position, size }: CellBaseProps) {
-    
   const [isHovered, setIsHovered] = useState(false);
   return (
     <mesh
@@ -83,10 +81,9 @@ function CellBase({ position, size }: CellBaseProps) {
   );
 }
 
-function Scene() {
+function Scene(children : any) {
   const camera = useRef<THREE.OrthographicCamera | null>(null);
   const controls = useRef<any>(null);
-  //   useHelper(camera, THREE.CameraHelper);
   useEffect(() => {
     if (camera.current && controls.current) {
       //Position camera above the XY plane
@@ -97,33 +94,13 @@ function Scene() {
       //Update Control
       camera.current.updateProjectionMatrix();
       controls.current.update();
-    //   console.log("hey this is afetr mount");
-    //   console.log(camera.current.position);
-    //   console.log(controls.current.target);
     }
   }, []);
 
   return (
     <>
-      <orthographicCamera
-        ref={camera}
-        position={[0, 10, 0]}
-        // lookAt={[0,-1,0]}
-        // makeDefault
-        left={-10}
-        right={10}
-        top={10}
-        bottom={-10}
-        near={0.1}
-        far={100}
-      />
-      <OrbitControls
-        ref={controls}
-        target={[0, 0, 0]} // Look at the origin
-        enablePan={true}
-        enableZoom={true}
-        enableRotate={false}
-      />
+      <orthographicCamera ref={camera}position={[0, 10, 0]}left={-10}right={10}top={10}bottom={-10}near={0.1}far={100}/>
+      <OrbitControls ref={controls} target={[0, 0, 0]} enablePan={true} enableZoom={true} enableRotate={false}/>
       <ambientLight intensity={2} color="white"></ambientLight>{" "}
       <CellBase position={[0, 0, 0]} size={[1, 1]}></CellBase>
       <CellBase position={[1, 0, 1]} size={[1, 1]}></CellBase>
@@ -132,13 +109,13 @@ function Scene() {
 }
 
 function Plot() {
-  
-  
+
+  // const canvas = useThree((state) => state.scene);
   return (
-    <Canvas camera={{ position: [0, 15, 0] }}>
-      <CursorCoords></CursorCoords>
-      <GridCells></GridCells>zcvcvczczxzxAZAA
-      <Scene></Scene>
+    <Canvas camera={{ position: [0, 15, 0] }} >
+      <CursorCoords/>
+      <GridCells/>
+      <Scene/>
     </Canvas>
   );
 }
