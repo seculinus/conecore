7a2bc85f88cf3ebd180af1baf92717ac96f25df5
seculinus <66112305+seculinus@users.noreply.github.com>
2025-09-16 00:33:01 +0400

    progress: coordinates & camera placement 1of2
    -problems with camera positioning, seems to orient to a diffent plane when the camera is moved

--- src/pages/plot/Plot.tsx
+++ src/pages/plot/Plot.tsx
@@ -6,35 +6,74 @@ import { OrbitControls, Html } from "@react-three/drei";
 import * as THREE from "three";
 import "./Plot.css";
 
+const points : Point[]= parsePoints();
+
+type Point ={
+  x: number;
+  y:number;
+  name:string;
+}
+
 type ThreeInput = [number, number, number];
 type CellBaseProps = {
   position: ThreeInput | THREE.Vector3;
   size: [width?: number, height?: number];
 };
 
-function handlePointClick() {
-  const points = parsePoints();
-  console.log(points);
+
+
+function MoveCamera(){
+  const {camera, size} = useThree();
+  const [isMoved, setMoved] = useState(false);
+  const pt = points[2];
+
+  const tagPosition = new THREE.Vector3(-5,5,0);
+
+  const handleClick = () => {
+    camera.position.set(pt.x,pt.y,10);
+    camera.lookAt(pt.x,pt.y,0);
+    setMoved(!isMoved);
+    console.log(camera)
+  }
+
+  return(
+    <Html  distanceFactor={400} position={camera.position}>
+      <div className="content" style={{width:40,height:20, background:'#202035', transform: 'translate3d(calc(50% + 40px), 0, 0)',  position:'absolute', left:50}}onClick={handleClick}>MC</div>
+    </Html>
+  )
 }
+
+
+function ReturnCameraXY(){}
+
+
+// function PointButton() {
+
+//   console.log(points);
+//   return (
+//     <Html>
+//       <div className = "btn-pt"style={{width:140,height:40 } } onClick={() =>
+//       {
+//         points = parsePoints();
+//         console.log(points)
+//       }
+//     }
+//       >PressMe
+//       </div>
+//     </Html>
+//   )
+
+// }
 
 function CellBase({ position, size }: CellBaseProps) {
   
   const [isHovered, setIsHovered] = useState(false);
-  const handlePointerOver = ()=>{setIsHovered(true)}
-  const handlePointerOut = ()=>{setIsHovered(false)}
+  const handlePointerOver = () => {setIsHovered(true)}
+  const handlePointerOut = () => {setIsHovered(false)}
+
 
   return (
     <mesh position={position} onPointerOver={handlePointerOver} onPointerOut={handlePointerOut} scale={isHovered ? 1.1 : 1}>
-      <Html distanceFactor={7} position={[0, 0, 0]}>
-        {isHovered ? 
-          <div className="content">
-            {position instanceof THREE.Vector3
-              ? `${(position.x, position.y, position.z)}`
-              : `${position[0]}${position[1]}${position[2]}`}
-          </div> :
-          <></>
-        }
-      </Html>
       <planeGeometry args={size}/>
       <meshStandardMaterial color={isHovered ? "#A1EED6" : "#F2B694"}/>
     </mesh>
@@ -44,6 +83,10 @@ function CellBase({ position, size }: CellBaseProps) {
 function Scene(children : any) {
   const camera = useRef<THREE.OrthographicCamera | null>(null);
   const controls = useRef<any>(null);
+
+  const cellbases = points.map((pos, i) =>{
+         return <CellBase  key = {i} position={[pos.x,pos.y, 0]} size={[1, 1]}></CellBase>
+       })
   useEffect(() => {
     if (camera.current && controls.current) {
       //Position camera above the XY plane
@@ -64,46 +107,52 @@ function Scene(children : any) {
       <ambientLight intensity={2} color="white"></ambientLight>{" "}
       <CellBase position={[0, 0, 0]} size={[1, 1]}></CellBase>
       <CellBase position={[1, 1, 0]} size={[1, 1]}></CellBase>
+      {cellbases}
     </>
   );
 }
 
 function Plot() {
   const container = useRef(null);
-
   return (
 
-    <Canvas camera={{ position: [0, 0, 10]} }  ref = {container}>
-        <GridCells/>
-        <CursorTracker/>
-        <Scene/>
-        
-    </Canvas>
+      <Canvas camera={{ position: [0, 0, 10]} }  ref = {container}>
+          <GridCells/>
+          <CursorTracker/>   
+          <Scene/>
+          <MoveCamera></MoveCamera>
+          {/* <PointButton></PointButton> */}
+      </Canvas>
 
-    
+
   );
 }
+
+
 interface Props{
   children?: ReactNode
 }
 
 
-
 function CursorTracker({children}:Props){
   const [position, setPosition] = useState(new THREE.Vector3(0, 0, 0));
+  const {camera}  = useThree();
+  const meshPos = new THREE.Vector3(0,0,-0.1);
   
+  console.log(camera.lookAt)
   return (
      <>
       <mesh 
-      position={[0,0,-0.1]} 
+      position={meshPos} 
       onPointerMove={(e)=>{setPosition(e.point)}}
       >
-        <planeGeometry args ={[2e6,2e6]}/>
+        <planeGeometry args ={[2e7,2e7]}/>
         <meshBasicMaterial transparent opacity={0}/>
       </mesh>
-      <Html className="content" distanceFactor={7} position={[0, 3, 3]}>
+      <Html className="content" distanceFactor={400} position={camera.position}>
         <div>
-          {`${(position.x).toFixed(2)}:${position.y.toFixed(2)}`}
+          <span style={{color: '#FFCCCB'}}>X:{(position.x).toFixed(2)}</span>
+          <span style = {{color:'#CCFEFF'}}>Y:{position.y.toFixed(2)}</span>
           {children}
         </div>
       </Html> 
--- src/utils/point.js
+++ src/utils/point.js
@@ -13,12 +13,14 @@ export function parsePoints(){
     for (let key of Object.keys(localStorage))
         {
             header = JSON.parse(localStorage.getItem(key));
-            
+            // console.log(header)
             const x = Number(header['E Coordinate']);
             const y = Number(header['N Coordinate']);
             const point = new Point(x,y, key);
+            // console.log(point)
             points.push(point);
         }
+    console.log("func exit")
     return points;
     
 }
